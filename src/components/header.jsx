3a50bad081be044bb9cb1ded7e40f5127fe6c241
SviatoslavJ <fwsviatoslav@gmail.com>
2024-08-06 22:38:41 +0200

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+
+
+export default function Header (){
+  return (
+    <div>
+      <header className="header">
+        <nav>
+          <ul className="headerList">
+            <li className="headerElement">Home</li>
+            <li className="headerElement">New films</li>
+            <li className="headerElement">Best 100</li>
+            <li className="headerElement">something</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
---
+++ src/components/slider.jsx
@@ -0,0 +1,19 @@
+
+
+export default function Slider(){
+
+
+
+
+  <div className="slider">
+    <div className="slides">
+      <div className="slide"><img src="" alt="Movie 1" /></div>
+      <div className="slide"><img src="" alt="Movie 2" /></div>
+      <div className="slide"><img src="" alt="Movie 3" /></div>
+      <div className="slide"><img src="" alt="Movie 4" /></div>
+      <div className="slide"><img src="" alt="Movie 5" /></div>
+    </div>
+    <button className="sliderPrev" onClick={prevSlide()}></button>
+    <button className="sliderNext" onClick={nextSlide()}></button>
+  </div>;
+}
