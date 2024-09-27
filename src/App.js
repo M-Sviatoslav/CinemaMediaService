81dc44cee2f0094796b3f2d2d1b048b8b9561093
SviatoslavJ <fwsviatoslav@gmail.com>
2024-09-27 16:29:43 +0200

    slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css"
 import Header from "./components/header";
+import FilmSlider from "./components/slider";
 
 export default function App() {
   return (
     <div className="App">
          <Header/>
+         <FilmSlider/>
     </div>
   );
 }
--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,19 +1,47 @@
+import React, { useState } from 'react';
 
 
-export default function Slider(){
+const FilmSlider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
+  const filmLogos = [
+    {
+      src: "/filmLogos/The_Fall_Guy_(2024)_poster.jpg",
+      alt: "Film 1",
+    },
+    {
+      src: "/filmLogos/Twisters2.jpg",
+      alt: "Film 2",
+    },
+    {
+      src: "/filmLogos/Road_House_2024.jpg",
+      alt: "Film 3",
+    },
+  ];
 
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % filmLogos.length);
+  };
 
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      (prevIndex - 1 + filmLogos.length) % filmLogos.length
+    );
+  };
 
-  <div className="slider">
-    <div className="slides">
-      <div className="slide"><img src="" alt="Movie 1" /></div>
-      <div className="slide"><img src="" alt="Movie 2" /></div>
-      <div className="slide"><img src="" alt="Movie 3" /></div>
-      <div className="slide"><img src="" alt="Movie 4" /></div>
-      
+  return (
+    <div className="slider-container">
+      <button onClick={prevSlide} className="slider-button">❮</button>
+      <div className="slider">
+        <img
+          src={filmLogos[currentIndex].src}
+          alt={filmLogos[currentIndex].alt}
+          className="slider-image"
+        />
+      </div>
+      <button onClick={nextSlide} className="slider-button">❯</button>
     </div>
-    <button className="sliderPrev" onClick={prevSlide()}></button>
-    <button className="sliderNext" onClick={nextSlide()}></button>
-  </div>;
-}
+  );
+};
+
+export default FilmSlider;
